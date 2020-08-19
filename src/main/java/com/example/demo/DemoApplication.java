27f4ac5d90f17c4db11b821818073ca2e0ffc2cc
nick.dai <daihuarong_2008@163.com>
2020-08-19 15:22:24 +0800

    测试2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("hello");
+        System.out.println("hello1");
+        System.out.println("hello2");
     }
 
 }
